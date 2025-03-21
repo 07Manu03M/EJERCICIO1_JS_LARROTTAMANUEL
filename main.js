98d4ae2a05ffa61587264de6ddef634ff62489b2
manuel meneses <manuelmeneses7200@gmail.com>
2025-03-21 12:56:30 -0500

    feat: :sparkles: readme added
    readme added

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-var ordenado = "({[]})"
+var inic = "({["
+var fin = "]})"
 
 function expresion(word){
     for (let i = 0; i < word.length; i++) {
-        if (word == ordenado){
+        if (word == inic + fin) {
             console.log("true")
         }else{
             console.log("false")
